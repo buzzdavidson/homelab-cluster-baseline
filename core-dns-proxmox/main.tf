1aa7e48a8e3d815543e0d012fc78c869f9f18fc8
Steve Davidson <buzzdata@gmail.com>
2023-12-07 00:39:02 -0800

    simplify cloud config and fix agent flag in template

--- core-dns-proxmox/main.tf
+++ core-dns-proxmox/main.tf
@@ -5,53 +5,13 @@ resource "proxmox_virtual_environment_pool" "operations_pool" {
 
 resource "proxmox_virtual_environment_file" "ubuntu_cloud_image" {
   content_type = "iso"
-  datastore_id = "local"
+  datastore_id = "nfs-flash"
   node_name    = "pve-04"
 
   source_file {
     path = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64-disk-kvm.img"
   }
 }
-
-resource "proxmox_virtual_environment_file" "bind9_cloud_config" {
-  # To clarify, this functionality uses SSH to connect to the host, as proxmox doesn't allow
-  # programmatic access to create snippets.
-  # FUTURE NOTE: because this uses SSH, the node name resolution is a bit weird.  This needs to be 
-  # a DNS-resolvable FQDN for the node, not just the node name.  This is most easily configured
-  # on the main provider definition.
-  #
-  # TODO: this doesn't work using storage nfs-flash, but does work using local storage
-  #
-  content_type = "snippets"
-  datastore_id = "local"
-  node_name    = "pve-04"
-
-  source_raw {
-    data = <<EOF
-#cloud-config
-chpasswd:
-  list: |
-    ubuntu:ubuntu
-  expire: false
-packages:
-  - qemu-guest-agent
-  - bind9
-  - bind9utils
-  - bind9-doc
-users:
-  - default
-  - name: ubuntu
-    groups: sudo
-    shell: /bin/bash
-    ssh-authorized-keys:
-      - "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDHvGD0vwY5rAR/TWkAEOmszyG45e6CLKnAYy5heBY9PQC/GgvbU7/q2ERNCClC3/wTvkyJNsloBHQwk7CabG2Y/6Glnsy6c1gbp8jl05aEupw5sFeKEzoW2GZ14AppV+2YjoZl6ufz3pgVcYI9qYzW3xzzv2tUMVCnUgoKJetL109zgA3DZontOcquRcwLmGJdmCZWbq0BSri/zSuRZ+rvAGakr0IVzPd11Iirx24xUUIXIytaDHiw5M34hlB/D9movcIJ5IFG5ezX8a5HyxPUiT0vvd5yBq+4Is0kAB5ZD0IsGP+V+iuBDcPcFi62C01IRLhuuOAw26Tfi7Wr2y37"
-    sudo: ALL=(ALL) NOPASSWD:ALL
-EOF
-
-    file_name = "ubuntu2204.bind9-cloud-config.yaml"
-  }
-}
-
 
 resource "proxmox_virtual_environment_vm" "dns_host_template" {
   vm_id       = 10000
@@ -65,7 +25,7 @@ resource "proxmox_virtual_environment_vm" "dns_host_template" {
   started     = false
 
   agent {
-    enabled = true
+    enabled = false
   }
   audio_device {
     enabled = false
@@ -89,12 +49,17 @@ resource "proxmox_virtual_environment_vm" "dns_host_template" {
     interface    = "scsi0"
   }
   initialization {
+    datastore_id = "nfs-flash"
     ip_config {
       ipv4 {
         address = "dhcp"
       }
     }
-    user_data_file_id = proxmox_virtual_environment_file.bind9_cloud_config.id
+    user_account {
+      username = "ubuntu"
+      password = "ubuntu"
+      keys     = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDHvGD0vwY5rAR/TWkAEOmszyG45e6CLKnAYy5heBY9PQC/GgvbU7/q2ERNCClC3/wTvkyJNsloBHQwk7CabG2Y/6Glnsy6c1gbp8jl05aEupw5sFeKEzoW2GZ14AppV+2YjoZl6ufz3pgVcYI9qYzW3xzzv2tUMVCnUgoKJetL109zgA3DZontOcquRcwLmGJdmCZWbq0BSri/zSuRZ+rvAGakr0IVzPd11Iirx24xUUIXIytaDHiw5M34hlB/D9movcIJ5IFG5ezX8a5HyxPUiT0vvd5yBq+4Is0kAB5ZD0IsGP+V+iuBDcPcFi62C01IRLhuuOAw26Tfi7Wr2y37"]
+    }
   }
   network_device {
     bridge  = "vmbr0"
@@ -111,11 +76,8 @@ resource "proxmox_virtual_environment_vm" "dns_01" {
   name        = "core-dns-01"
   node_name   = "pve-04"
   description = "Managed by Terraform"
-  #  tags        = ["terraform", "ubuntu", "coreservices"]
-  #  pool_id     = proxmox_virtual_environment_pool.operations_pool.id
-  #  template    = true
-  reboot  = true
-  started = true
+  reboot      = true
+  started     = true
   clone {
     full  = true
     vm_id = proxmox_virtual_environment_vm.dns_host_template.id
@@ -135,11 +97,8 @@ resource "proxmox_virtual_environment_vm" "dns_02" {
   name        = "core-dns-02"
   node_name   = "pve-04"
   description = "Managed by Terraform"
-  #  tags        = ["terraform", "ubuntu", "coreservices"]
-  #  pool_id     = proxmox_virtual_environment_pool.operations_pool.id
-  #  template    = true
-  reboot  = true
-  started = true
+  reboot      = true
+  started     = true
   clone {
     full  = true
     vm_id = proxmox_virtual_environment_vm.dns_host_template.id
