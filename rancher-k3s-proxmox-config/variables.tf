e7f459f520bd8f4e13718bd7df5f004e8e406639
Steve Davidson <buzzdata@gmail.com>
2024-04-12 16:46:14 -0700

    Remove unused module

--- rancher-k3s-proxmox-config/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "domain_ntp_server" {
-  type        = string
-  description = "NTP server for domain"
-}
-
-variable "domain_fallback_ntp_server" {
-  type        = string
-  description = "Fallback NTP server for domain"
-}
